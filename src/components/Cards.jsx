090292b15636208291c97378203a9214f476dfac
Evelyn-Nobile <nobileevelyn1@gmail.com>
2023-09-06 01:59:47 -0300

    ESTADOS OK

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,6 @@
 import Card from "./Card";
 
-const Cards= ({ characters}) => {
+const Cards= ({ characters, onClose}) => {
 
   return (
     <div>
@@ -14,7 +14,7 @@ const Cards= ({ characters}) => {
           gender={gender}
           origin={origin.name}
           image={image}
-          onClose={() => alert('Emulamos que se cierra la card')}
+          onClose={onClose}
         />
       ))}
     </div>
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
+
 const SearchBar = ({onSearch}) => {
-  return (
-     <div>
-         <input type='search' />
-        <button onClick={onSearch}>Agregar</button> 
-     </div>
-  );
-}
+   let [id,setId] = useState('');
+
+   const handleChange = (event) =>{
+   setId(event.target.value)
+   }
+
+
+   return (
+      <div>
+        <input type="search" value={id} onChange={handleChange}/>
+        <button onClick={() => {onSearch(id); setId('')}}>Agregar</button> {/*primero me trae la info del personaje y despues me borra el input*/}
+      </div>
+   )
+};
+
 
 export default SearchBar;
