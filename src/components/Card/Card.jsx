10a7c29c2d56f73a96f21de863bff943e7bb0829
Evelyn-Nobile <nobileevelyn1@gmail.com>
2023-09-14 22:41:03 -0300

    se cambio el color de las letras para que se vea en la entrega'

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { addFav, removeFav } from "../../redux/actions";
 import { connect } from "react-redux";
 import { useState,useEffect } from "react";
+import style from "./Card.module.css"
 
 
 
@@ -36,10 +37,12 @@ const Card = ({id,name,species,gender,image,onClose,addFav,removeFav,myFavorites
 
       <Link to={`/detail/${id}`}>
         <h2 className="card-name">{name}</h2>
-      </Link>     
+      </Link>  
+      <div className={style.cardcontainer}>
       <h3>{species}</h3>
       <h3>{gender}</h3>
       <img src={image} alt="" />
+      </div>   
     </div>
   );
 };
